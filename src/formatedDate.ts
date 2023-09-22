680736529ab20db5a97677665565206647c361eb
a1ienist <138213365+a1ienist@users.noreply.github.com>
2023-09-22 19:37:53 +0300

    2

--- src/formatedDate.ts
+++ src/formatedDate.ts
@@ -1,11 +1,11 @@
 const now = new Date();
-    const year = now.getFullYear();
-    const month = String(now.getMonth() + 1).padStart(2, '0'); // +1, так как месяцы начинаются с 0
-    const day = String(now.getDate()).padStart(2, '0');
-    const hours = String(now.getHours()).padStart(2, '0');
-    const minutes = String(now.getMinutes()).padStart(2, '0');
+const year: number = now.getFullYear();
+const month: string = String(now.getMonth() + 1).padStart(2, '0'); 
+const day: string = String(now.getDate()).padStart(2, '0');
+const hours: string = String(now.getHours()).padStart(2, '0');
+const minutes: string = String(now.getMinutes()).padStart(2, '0');
     
-    const formattedDateTime = `${year}-${month}-${day} ${hours}:${minutes}`;
-    console.log(formattedDateTime);
+const formattedDateTime: string = `${year}-${month}-${day} ${hours}:${minutes}`;
+console.log(formattedDateTime);
 
-export {formattedDateTime};
+export { formattedDateTime };
